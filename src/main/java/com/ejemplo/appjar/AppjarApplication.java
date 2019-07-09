f26d813da725d0cd06400f21b69352ccccaf9e46
bhgonzales <bhgonzales@umsa.bo>
2019-07-09 09:09:57 -0500

    security

--- src/main/java/com/ejemplo/appjar/AppjarApplication.java
+++ src/main/java/com/ejemplo/appjar/AppjarApplication.java
@@ -1,11 +1,20 @@
 package com.ejemplo.appjar;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppjarApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AppjarApplication.class, args);
+		// SpringApplication.run(AppjarApplication.class, args);
+		SpringApplication app = new SpringApplication(AppjarApplication.class);
+
+		Properties properties = new Properties();
+		properties.setProperty("spring.resources.static-locations",
+				"classpath:/view/");
+		app.setDefaultProperties(properties);
+		app.run(args);
 	}
 }
